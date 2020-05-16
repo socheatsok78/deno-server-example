4c1ab4a8c52f0f7f3f2e19b1e8c28f39ed915791
Socheat Sok <alex@socheat.net>
2020-05-16 13:56:35 +0700

    Update src/server.ts and add Makefile

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
-DENO := deno --allow-net
+DENO ?= deno
+DENO_PERMISSIONS ?= --allow-net
+
+SERVER_INDEX = src/server.ts
 
 server:
-    @echo "Starting up..."
-    @${DENO} src/server.ts
+	@echo "Starting up..."
+	@${DENO} ${DENO_PERMISSIONS} ${SERVER_INDEX}
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std@0.50.0/http/server.ts";
 
 const _PORT = 8080;
 const s = serve({ port: _PORT });
-console.log(`http://localhost:${_PORT}/`);
+console.log(`App run at: http://localhost:${_PORT}/`);
 
 for await (const req of s) {
   req.respond({ body: "Hello World\n" });
